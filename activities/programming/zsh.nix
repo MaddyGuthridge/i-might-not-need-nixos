06551cff08854012fafb0e503a8c802ee7989455
Maddy Guthridge <hello@maddyguthridge.com>
2025-11-19 15:04:35 +1100

    Document zsh in /etc/shells

--- activities/programming/zsh.nix
+++ activities/programming/zsh.nix
@@ -2,5 +2,8 @@
 {
   programs.zsh.enable = true;
   users.users.maddy.shell = pkgs.zsh;
+  # The docs warn that failure to add zsh to /etc/shells will make Gnome's login
+  # manager refuse to display your account. This didn't appear to be the case in
+  # my testing, but it's better safe than sorry.
   environment.shells = with pkgs; [ zsh ];
 }
