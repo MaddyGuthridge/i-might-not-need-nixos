02e0d3d3b2fbe0fde2fb1e7a5481ef40efd4ca41
Maddy Guthridge <hello@maddyguthridge.com>
2025-11-21 16:03:13 +1100

    Start work on slides?

---
+++ presentation/01.boot.slide.py
@@ -0,0 +1,9 @@
+from slides_xp.themes.xp import make_intro_slide
+
+
+def render():
+    return make_intro_slide(
+        "Maddy",
+        "LinSoc Presentation",
+        "Maddy Guthridge",
+    )
